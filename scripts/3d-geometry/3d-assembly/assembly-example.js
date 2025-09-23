893a96323277fb28e9456520c786cbaea5aaa3d6
Tore Hvidegaard <thv@3dbyggeri.dk>
2025-09-23 13:10:09 +0200

    Adding v1 of Profile setup

--- scripts/3d-geometry/3d-assembly/assembly-example.js
+++ scripts/3d-geometry/3d-assembly/assembly-example.js
@@ -1,5 +1,5 @@
 import { Group } from "three";
-import { createExtrudedProfile } from "../3d-parts/extruded-profile-example";
+import { profileXXXXX } from "../3d-parts/extruded-profile-example";
 
 //This is file where all parts are being assembled into one group
 export function createAssembly (opt) {
@@ -7,9 +7,11 @@ export function createAssembly (opt) {
 
   //create assembly of all elements
   const assemblyGroup = new Group();
+
+  const profileLength = 10;
   
   //create geometry
-  const extrudedProfile = createExtrudedProfile();
+  const extrudedProfile = profileXXXXX(profileLength);
 
   //possible to add multiple geometries to assembly group
   assemblyGroup.add(
--- scripts/3d-geometry/3d-parts/extruded-profile-example.js
+++ scripts/3d-geometry/3d-parts/extruded-profile-example.js
@@ -1,60 +1,48 @@
-import { Shape, ExtrudeGeometry, Mesh } from "three";
+import { Shape, ExtrudeGeometry, Mesh, Group } from "three";
 import { profileTestMaterial } from "../materials";
 
 // parameters
-const extrusionDepthExample = 10;
+const defaultProfileMaterial = profileTestMaterial
 
-function createProfileP1749() {
-  const shape = new Shape();
-  shape.moveTo(0, 0);
-  shape.lineTo(3, 0);
-  shape.lineTo(3, -14);
-  shape.lineTo(17, -14);
-  shape.lineTo(17, 0);
-  shape.lineTo(20, 0);
-  shape.lineTo(20, -30);
-  shape.lineTo(18, -30);
-  shape.lineTo(18, -17);
-  shape.lineTo(2, -17);
-  shape.lineTo(2, -30);
-  shape.lineTo(0, -30);
-  shape.lineTo(0, 0);
-
-  return shape;
-}
-
-function createProfileP1609() {
-  const shape = new Shape();
-  shape.moveTo(0, 0);
-  shape.lineTo(3, 0);
-  shape.lineTo(3, -14);
-  shape.lineTo(17, -14);
-  shape.lineTo(17, 0);
-  shape.lineTo(20, 0);
-  shape.lineTo(20, -50);
-  shape.lineTo(18, -50);
-  shape.lineTo(18, -17);
-  shape.lineTo(2, -17);
-  shape.lineTo(2, -50);
-  shape.lineTo(0, -50);
-  shape.lineTo(0, 0);
+function createProfileP1827_1829_1830() {
+  const shape = new Shape()
+   .moveTo(0, 0)
+    .lineTo(56, 0)
+    .lineTo(56, -16.5)
+    .lineTo(42, -16.5)
+    .lineTo(42, -14.5)
+    .lineTo(53.5, -14.5)
+    .lineTo(53.5, -3)
+    .lineTo(19, -3)
+    .lineTo(19, -37.8)
+    .lineTo(56, -37.8)
+    .lineTo(56, -40)
+    .lineTo(16, -40)
+    .lineTo(16, -4)
+    .lineTo(0, -4)
+    .lineTo(0, 0);
 
   return shape;
 }
 
-export function createExtrudedProfile() {
-  //const shape = createProfileP1749();
-  const shape = createProfileP1609();
+export function profileXXXXX(profileLength) {
+  const shape = createProfileP1827_1829_1830();
 
   const extrudeSettings = {
-    depth: extrusionDepthExample,
+    depth: profileLength,
     bevelEnabled: false,
   };
 
   const geometry = new ExtrudeGeometry(shape, extrudeSettings);
-  const material = profileTestMaterial;
+  const material = defaultProfileMaterial;
 
   const mesh = new Mesh(geometry, material);
+  mesh.position.set(61, 0, 0);
+  mesh.rotateX(Math.PI / 2);
+  mesh.rotateZ(Math.PI);
+  
+  const meshgroup = new Group
+  meshgroup.add(mesh)
 
-  return mesh;
+  return meshgroup;
 }
